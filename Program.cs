c0e17afea6766e57814af62fad7d907f8cb16b18
michi <bergen.michael@web.de>
2026-01-02 17:24:39 +0100

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Methode_Ubergabewert001
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Rechteck rechteck = new Rechteck();
+            rechteck.A = int.Parse(Console.ReadLine());
+            rechteck.B = int.Parse(Console.ReadLine());
+            Console.WriteLine(rechteck.getflache());
+        }
+        public class Rechteck
+        {
+            public int A { get; set; }
+            public int B { get; set; }
+            public int Flache { get; set; }
+            public int getflache()
+            {
+
+                Flache = A * B;
+                return Flache;
+            }
+        }
+    }
+}
+
